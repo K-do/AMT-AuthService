7c3681f2d54e9293521bbc5b45c416a4f7606ac1
Nicolas Ogi <nicolas.ogi@heig-vd.ch>
2021-12-17 17:49:39 +0100

    Setup sign algorithm for JWT

--- src/main/java/ch/heigvd/amt/resources/AuthResource.java
+++ src/main/java/ch/heigvd/amt/resources/AuthResource.java
@@ -5,6 +5,9 @@ import ch.heigvd.amt.services.UserService;
 import com.fasterxml.jackson.databind.node.JsonNodeFactory;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import io.quarkus.elytron.security.common.BcryptUtil;
+import io.smallrye.jwt.algorithm.SignatureAlgorithm;
+import io.smallrye.jwt.build.Jwt;
+
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import javax.ws.rs.Consumes;
@@ -40,14 +43,16 @@ public class AuthResource {
       User userLoggingIn = userService.getUser(receivedUser.getUsername()).get();
 
       // Check if the hashes match
-
       if (BcryptUtil.matches(receivedUser.getPassword(), userLoggingIn.getPassword())) {
 
         // Creation of the JWT
-        /*String jwt =
-        Jwt.issuer("AMT-AuthService").groups(userLoggingIn.getRole().toString()).sign();*/
+        String jwt =
+        Jwt.issuer("AMT-AuthService").groups(userLoggingIn.getRole().toString())
+                .jws()
+                .algorithm(SignatureAlgorithm.ES256)
+                .sign();
 
-        responseBody.put("token", "jwt");
+        responseBody.put("token", jwt);
         responseBody
             .putObject("account")
             .put("username", userLoggingIn.getUsername())
@@ -55,7 +60,6 @@ public class AuthResource {
         return Response.status(Response.Status.OK).entity(responseBody).build();
       }
     }
-
     responseBody.put("error", CREDENTIALS_ERROR);
     return Response.status(Response.Status.FORBIDDEN).entity(responseBody).build();
   }
